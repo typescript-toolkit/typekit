9796faf163b9631f5b59830f4dc4c53854219896
pekochan069 <pekochan069@gmail.com>
2026-02-23 23:55:10 +0900

    feat: throw when trying to add tag to non extensible object

--- packages/typekit/src/tagged/implementation.ts
+++ packages/typekit/src/tagged/implementation.ts
@@ -8,6 +8,13 @@ export const tagged: {
   <Tag extends string, T extends object>(object: T, tag: Tag): T & Tagged<Tag>;
   <Tag extends string>(tag: Tag): <T extends object>(object: T) => T & Tagged<Tag>;
 } = dual(2, function <Tag extends string, T extends object>(object: T & { _tag?: never }, tag: Tag): T & Tagged<Tag> {
+  if (!Object.isExtensible(object)) {
+    throw new TypeError("tagged: object is not extensible");
+  }
+  if ("_tag" in object) {
+    throw new TypeError("tagged: object already has '_tag'");
+  }
+
   return Object.defineProperty(object, "_tag", {
     value: tag,
     writable: false,
