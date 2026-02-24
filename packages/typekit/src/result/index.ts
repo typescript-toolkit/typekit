3bb5e04e729c551dc4fe8f461a6fe19f4912eda2
pekochan069 <97679910+pekochan069@users.noreply.github.com>
2026-02-24 12:45:17 +0900

    feat: re-export individual functions from implementation

--- packages/typekit/src/result/index.ts
+++ packages/typekit/src/result/index.ts
@@ -1,1 +1,2 @@
 export * as Result from "./implementation";
+export { ok, err, isOk, isErr, unwrap, unwrapOr, unwrapErr, unwrapErrOr, expect, flatten, map, mapErr, biMap, flatMap, flatMapErr, biFlatMap } from "./implementation";
