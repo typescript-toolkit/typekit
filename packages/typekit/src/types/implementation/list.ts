e46f93de91a413041574bdc6d84a1095150a1098
pekochan069 <pekochan069@gmail.com>
2026-03-02 00:23:08 +0900

    feat: Create Head type

--- packages/typekit/src/types/implementation/list.ts
+++ packages/typekit/src/types/implementation/list.ts
@@ -1,5 +1,15 @@
 export type ListLike<T> = [] | readonly [] | T[] | readonly T[];
 
+export type Head<T extends ListLike<unknown>> = T extends []
+  ? never
+  : T extends readonly []
+    ? never
+    : T extends [infer Head, ...unknown[]]
+      ? Head
+      : T extends readonly [infer Head, ...unknown[]]
+        ? Head
+        : never;
+
 export type Tail<T extends ListLike<unknown>> = T extends []
   ? []
   : T extends readonly []
